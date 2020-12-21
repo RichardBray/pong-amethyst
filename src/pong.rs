e7106d625ac5992411fe1d884552ee8b342abfcf
Richard Bray <richardbray89@gmail.com>
2020-12-21 19:35:26 +0000

    chore: add paddle to pong game

--- src/pong.rs
+++ src/pong.rs
@@ -6,9 +6,23 @@ use amethyst::{
   renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
 };
 
-pub struct Pong;
 pub const ARENA_HEIGHT: f32 = 100.0;
 pub const ARENA_WIDTH: f32 = 100.0;
+pub const PADDLE_HEIGHT: f32 = 16.0;
+pub const PADDLE_WIDTH: f32 = 4.0;
+
+pub enum Side {
+  Left,
+  Right,
+}
+
+pub struct Pong;
+
+pub struct Paddle {
+  pub side: Side,
+  pub width: f32,
+  pub height: f32,
+}
 
 fn initialise_camera(world: &mut World) {
   // Setup camera in a way that our screen covers whole arena and (0, 0) is in the bottom left.
@@ -23,9 +37,76 @@ fn initialise_camera(world: &mut World) {
       .build();
 }
 
+// Initialises one paddle on the left, and one paddle on the right
+fn initialise_paddles(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
+  let mut left_transform = Transform::default();
+  let mut right_transform = Transform::default();
+
+  // Correctly position the paddles
+  let y = ARENA_HEIGHT / 2.0;
+  left_transform.set_translation_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+  right_transform.set_translation_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);  
+
+  // Assign the sprites for the paddles
+  let sprite_render = SpriteRender::new(sprite_sheet_handle, 0);  // paddle is the first sprite in the sprite_sheet
+
+  // Create a left plank entity
+  world
+    .create_entity()
+    .with(Paddle::new(Side::Left))
+    .with(left_transform)
+    .build();
+
+  // Create right plank entity.
+  world
+    .create_entity()
+    .with(Paddle::new(Side::Right))
+    .with(right_transform)
+    .build();    
+}
+
+fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+  let texture_handle = {
+    let loader = world.read_resource::<Loader>();
+    let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+    loader.load(
+      "texture/pong_spritesheet.png",
+      ImageFormat::default(),
+      (),
+      &texture_storage,
+    )
+  };
+
+  let loader = world.read_resource::<Loader>();
+  let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+  loader.load(
+      "texture/pong_spritesheet.ron", // Here we load the associated ron file
+      SpriteSheetFormat(texture_handle),
+      (),
+      &sprite_sheet_store,
+  )  
+}
+
+impl Paddle {
+  fn new(side: Side) -> Paddle {
+    Paddle {
+      side,
+      width: PADDLE_WIDTH,
+      height: PADDLE_HEIGHT,
+    }
+  }
+}
+
+impl Component for Paddle {
+  type Storage = DenseVecStorage<Self>;
+}
+
 impl SimpleState for Pong {
   fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+    let world = data.world;
 
+    world.register::<Paddle>();
+    initialise_paddles(world);
+    initialise_camera(world);
   }
-  
 }
